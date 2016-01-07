47aef3d19710df3f5b2159b1bd5cba90e1b19b7c
neu5 <neus90@gmail.com>
2016-01-07 22:45:18 +0100

    add `smooth scroll`

--- js/main.js
+++ js/main.js
@@ -26,4 +26,13 @@
   }, 250);
 
   window.addEventListener('scroll', scrollHandler);
+
+  smoothScroll.init({
+    selector: '[data-scroll]', // Selector for links (must be a valid CSS selector)
+    selectorHeader: '[data-scroll-header]', // Selector for fixed headers (must be a valid CSS selector)
+    speed: 500, // Integer. How fast to complete the scroll in milliseconds
+    easing: 'easeInOutQuad', // Easing pattern to use
+    updateURL: true, // Boolean. Whether or not to update the URL with the anchor hash on scroll
+    offset: 0 // Integer. How far to offset the scrolling anchor location in pixels
+  });
 })();
