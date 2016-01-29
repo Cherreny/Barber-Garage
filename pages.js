8da8461ec36cb2924030067b2066473dd8057306
neu5 <neus90@gmail.com>
2016-01-29 18:41:28 +0100

    add express server

---
+++ pages.js
@@ -0,0 +1,15 @@
+module.exports = [
+  {
+    fileName: 'index.html',
+    pageName: 'homepage.ejs',
+    uri: '/'
+  }, {
+    fileName: 'barber.html',
+    pageName: 'barber.ejs',
+    uri: '/barber'
+  }, {
+    fileName: 'garage.html',
+    pageName: 'garage.ejs',
+    uri: '/garage'
+  }
+];
---
+++ server.js
@@ -0,0 +1,59 @@
+// server.js
+// load the things we need
+var express = require('express');
+var ejs = require('ejs');
+var fs = require('fs');
+var path = require('path');
+
+var pages = require('./pages');
+
+var data = {
+  
+};
+
+var timestamp = new Date().getTime();
+
+var app = express();
+
+var distPath = 'dist';
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.use('/', express.static('public'));
+
+// loop for default uris
+pages.forEach(page => {
+  app.get(page.uri, (req, res) => {
+    res.render('layout', {
+      content: getPageContent(path.join('pages', page.pageName), data[page.data])
+    });
+  });
+});
+
+app.get('/build', (req, res) => {
+  pages.forEach(page => {
+    var fileName = path.join(distPath, page.fileName);
+    var stream = fs.createWriteStream(fileName);
+
+    stream.once('open', fd => {
+      var html = buildHtml(page.pageName, data[page.data]);
+      stream.end(html);
+    });
+  });
+
+  res.send('build succeeded');
+});
+
+app.listen(8080);
+console.log('server is running on localhost:8080');
+
+function getPageContent(fileName, data) {
+  return ejs.render(fs.readFileSync(path.join('views', fileName), 'utf-8'), {data: data, timestamp: timestamp});
+}
+
+function buildHtml(pageName, data) {
+  return getPageContent('partials/head.ejs') +
+         getPageContent(path.join('pages', pageName), data) +
+         getPageContent('partials/footer.ejs');
+}
