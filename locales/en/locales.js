d3e5c226fba72716510f1dfa878134d8c30b2005
Cherreny <kinga9090@gmail.com>
2016-02-27 19:06:49 +0100

    adding english version to subpages

--- locales/en/locales.js
+++ locales/en/locales.js
@@ -2,6 +2,9 @@ module.exports = {
   'reservations': 'Reservations',
   'read more': 'read more',
   homepage: {
-    barber: 'Barbershop is more than a place to have a haircut, to have your beard trimmed, or shaved with a shavette razor. It is a male micro-community for exchanging opinions, views  and social meetings&hellip;'
+    barber: 'Barbershop is more than a place to have a haircut, to have your beard trimmed, or shaved with a shavette razor. It is a male micro-community for exchanging opinions, views  and social meetings&hellip;',
+    garage: 'Barber Garage is more than a barbershop. In our Garage at Modlińska Street you can find the full range of car repairing services&hellip;'
   }
+  barber: 'Barbershop is more than a place to have a haircut, to have your beard trimmed, or shaved with a shavette razor. It is a male micro-community for exchanging opinions, views  and social meetings. It is an hour when you can get laid back and talk to a barber or to other customers. Time spent in the garage is a relaxing while for taking care of yourself with sounds of ambient music - it is just time for you.',
+  garage: 'Barber Garage is more than a barbershop. In our Garage at Modlińska Street you can find the full range of car repairing services. Moreover, we construct on demand stylish bicycles, inspired by the design of motorbikes from the early 20th Century. Our specialists also reconstruct classical motorbikes into boober style and cafe racer or just renovate old ones.',
 };
--- locales/pl/locales.js
+++ locales/pl/locales.js
@@ -2,6 +2,9 @@ module.exports = {
   'reservations': 'Rezerwuj',
   'read more': 'dowiedz się więcej',
   homepage: {
-    barber: 'Barbershop to coś więcej niż miejsce w którym możesz przyjść na strzyżenie włosów, trymowanie brody czy też golenie zarostu brzytwą. Jest ośrodkiem mikrospołeczności mężczyzn, miejscem wymiany poglądów i spotkań towarzyskich&hellip;'
+    barber: 'Barbershop to coś więcej niż miejsce w którym możesz przyjść na strzyżenie włosów, trymowanie brody czy też golenie zarostu brzytwą. Jest ośrodkiem mikrospołeczności mężczyzn, miejscem wymiany poglądów i spotkań towarzyskich&hellip;',
+    garage: 'Barber Garage to coś więcej niż barbershop. W naszym garażu na ulicy Modlińskiej znajdziesz pełen zakres usług związanych z szeroko pojętą mechaniką samochodową&hellip;'
   }
+  barber: 'Barbershop to coś więcej niż miejsce w którym możesz przyjść na strzyżenie włosów, trymowanie brody czy też golenie zarostu brzytwą. Jest ośrodkiem mikrospołeczności mężczyzn, miejscem wymiany poglądów i spotkań towarzyskich. To godzina, w której podczas zabiegów możesz się odprężyć, zrelaksować i porozmawiać z barberem, czy też z innymi klientami. Czas spędzony w barberze jest chwilą relaksu przy dobrej muzyce i zadbania o swój wygląd – to czas dla Ciebie.',
+  garage: 'Barber Garage to coś więcej niż barbershop. W naszym garażu na ulicy Modlińskiej znajdziesz pełen zakres usług związanych z szeroko pojętą mechaniką samochodową. Dodatkowo na specjalne zamówienie kienta składamy stylowe rowery, które charakteryzują sie wyglądem motocykli z początku XX wieku. Ponad to nasi mechanicy zajmują się przebótową klasycznych motocykli na styl boober, cafe racer oraz renowacją starych motocykli.',
 };
