2dda74e5532ccbca690905f06a2ecad4c3ca1944
Cherreny <kinga9090@gmail.com>
2016-02-27 20:27:21 +0100

    adding english version to footer

--- locales/en/locales.js
+++ locales/en/locales.js
@@ -5,6 +5,9 @@ module.exports = {
     barber: 'Barbershop is more than a place to have a haircut, to have your beard trimmed, or shaved with a shavette razor. It is a male micro-community for exchanging opinions, views  and social meetings&hellip;',
     garage: 'Barber Garage is more than a barbershop. In our Garage at Modlińska Street you can find the full range of car repairing services&hellip;'
   },
+  footer: {
+    open: ' Mon - Fr 12.00 - 20.00<br />St 10.00 - 16.00'
+  },
   barber: 'Barbershop is more than a place to have a haircut, to have your beard trimmed, or shaved with a shavette razor. It is a male micro-community for exchanging opinions, views  and social meetings. It is an hour when you can get laid back and talk to a barber or to other customers. Time spent in the garage is a relaxing while for taking care of yourself with sounds of ambient music - it is just time for you.',
   garage: 'Barber Garage is more than a barbershop. In our Garage at Modlińska Street you can find the full range of car repairing services. Moreover, we construct on demand stylish bicycles, inspired by the design of motorbikes from the early 20th Century. Our specialists also reconstruct classical motorbikes into boober style and cafe racer or just renovate old ones.',
 };
