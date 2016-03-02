8c51c46dca01629d7ff27dae7545eb65fd571c89
Jakub Kasprzyk <neus90@gmail.com>
2016-03-02 15:51:23 +0100

    add `price list` translation

--- locales/en/locales.js
+++ locales/en/locales.js
@@ -8,6 +8,89 @@ module.exports = {
     garage: 'Barber Garage is more than a barbershop. In our Garage at Modlińska Street you can find the full range of car repairing services&hellip;'
   },
   barber: 'Barbershop is more than a place to have a haircut, to have your beard trimmed, or shaved with a shavette razor. It is a male micro-community for exchanging opinions, views  and social meetings. It is an hour when you can get laid back and talk to a barber or to other customers. Time spent in the garage is a relaxing while for taking care of yourself with sounds of ambient music - it is just time for you.',
+  'price list': `<label for="price-list">PRICE LIST</label>
+  <table id="price-list">
+    <tr>
+      <td>
+        haircut - <strong>OLD DICK</strong>
+      </td>
+      <td>
+        <strong>100,-</strong>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        haircut - <strong>YOUNG BOYS </strong>(do 12 lat)
+      </td>
+      <td>
+        <strong>50,-</strong>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        haircut - <strong>0,3 mm</strong>
+      </td>
+      <td>
+        <strong>30,-</strong>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        haircut - <strong>0,3 mm + shavette contour</strong>
+      </td>
+      <td>
+        <strong>50,-</strong>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <strong>head shaving with a shavette</strong>
+      </td>
+      <td>
+        <strong>100,-</strong>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        beard trimming <strong>hair clipper</strong>
+      </td>
+      <td>
+        <strong>40,-</strong>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        beard trimming <strong>shavette contour </strong>
+      </td>
+      <td>
+        <strong>60,-</strong>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <strong>full shavette shaving</strong>
+      </td>
+      <td>
+        <strong>80,-</strong>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        set: <strong>hear + beard</strong>
+      </td>
+      <td>
+        <strong>120,-</strong>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        set: <strong>hair + beard + shavette contour</strong>
+      </td>
+      <td>
+        <strong>140,-</strong>
+      </td>
+    </tr>
+  </table>`,
   garage: 'Barber Garage is more than a barbershop. In our Garage at Modlińska Street you can find the full range of car repairing services. Moreover, we construct on demand stylish bicycles, inspired by the design of motorbikes from the early 20th Century. Our specialists also reconstruct classical motorbikes into boober style and cafe racer or just renovate old ones.',
   footer: {
     open: 'Mon - Fr 12.00 - 20.00<br />St 10.00 - 16.00'
--- locales/pl/locales.js
+++ locales/pl/locales.js
@@ -8,6 +8,89 @@ module.exports = {
     garage: 'Barber Garage to coś więcej niż barbershop. W naszym garażu na ulicy Modlińskiej znajdziesz pełen zakres usług związanych z szeroko pojętą mechaniką samochodową&hellip;'
   },
   barber: 'Barbershop to coś więcej niż miejsce w którym możesz przyjść na strzyżenie włosów, trymowanie brody czy też golenie zarostu brzytwą. Jest ośrodkiem mikrospołeczności mężczyzn, miejscem wymiany poglądów i spotkań towarzyskich. To godzina, w której podczas zabiegów możesz się odprężyć, zrelaksować i porozmawiać z barberem, czy też z innymi klientami. Czas spędzony w barberze jest chwilą relaksu przy dobrej muzyce i zadbania o swój wygląd – to czas dla Ciebie.',
+  'price list': `<label for="price-list">CENNIK</label>
+  <table id="price-list">
+    <tr>
+      <td>
+        strzyżenie - <strong>OLD DICK</strong>
+      </td>
+      <td>
+        <strong>100,-</strong>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        strzyżenie - <strong>YOUNG BOYS </strong>(do 12 lat)
+      </td>
+      <td>
+        <strong>50,-</strong>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        strzyżenie - <strong>0,3 mm</strong>
+      </td>
+      <td>
+        <strong>30,-</strong>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        strzyżenie - <strong>0,3 mm + KONTUR BRZYTWĄ</strong>
+      </td>
+      <td>
+        <strong>50,-</strong>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <strong>GOLENIE GŁOWY BRZYTWĄ</strong>
+      </td>
+      <td>
+        <strong>100,-</strong>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        trymowanie brody <strong>MASZYNKA</strong>
+      </td>
+      <td>
+        <strong>40,-</strong>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        trymowanie brody <strong>KONTUR BRZYTWĄ</strong>
+      </td>
+      <td>
+        <strong>60,-</strong>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <strong>PEŁNE GOLENIE BRZYTWĄ</strong>
+      </td>
+      <td>
+        <strong>80,-</strong>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        komplet: <strong>WŁOSY + BRODA</strong>
+      </td>
+      <td>
+        <strong>120,-</strong>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        komplet: <strong>WŁOSY + BRODA + KONTUR BRZYTWĄ</strong>
+      </td>
+      <td>
+        <strong>140,-</strong>
+      </td>
+    </tr>
+  </table>`,
   garage: 'Barber Garage to coś więcej niż barbershop. W naszym garażu na ulicy Modlińskiej znajdziesz pełen zakres usług związanych z szeroko pojętą mechaniką samochodową. Dodatkowo na specjalne zamówienie kienta składamy stylowe rowery, które charakteryzują sie wyglądem motocykli z początku XX wieku. Ponad to nasi mechanicy zajmują się przebótową klasycznych motocykli na styl boober, cafe racer oraz renowacją starych motocykli.',
   footer: {
     open: 'pon - pt 12.00 - 20.00<br/>so 10.00 - 16.00'
